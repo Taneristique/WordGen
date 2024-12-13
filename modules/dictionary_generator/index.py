4c87857c3f937bd58885cfb7029c55ac84a37846
Taneristique <48108872+Taneristique@users.noreply.github.com>
2024-12-13 12:50:06 +0300

    Update index.py

--- modules/dictionary_generator/index.py
+++ modules/dictionary_generator/index.py
@@ -4,12 +4,12 @@ class WordGen:
         if length <= 2 or length > 64:
             raise ValueError("Length should be between 2 and 65!")
         self.count= 100000
-        self.c =''
+        self.c ='' # it will store the words of the dictionary 
         if(sw == True):
             self.c = [chr(i) for i in range(97,123)] + [str(j) for j in range(10)]+['?','.','!','*']
         else:
             self.c = [chr(i) for i in range(97,123)] + [str(j) for j in range(10)]
-        self.length = length
+        self.length = length # character length
         self.words = self.generate()
     def generate(self):
         words = ""
